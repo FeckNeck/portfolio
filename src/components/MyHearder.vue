386aee9b953d39b0be896ace6a282a5b0860bb1f
DOUSSE MATHIS p1802296 <mathis.dousse@etu.univ-lyon1.fr>
2022-09-06 12:39:42 +0200

    fix: fautes d'orthographe

--- src/components/MyHearder.vue
+++ src/components/MyHearder.vue
@@ -83,19 +83,19 @@
             "
             >Présentation</router-link
           >
-          <router-link
-            :to="{ name: 'Projects', hash: '#projects' }"
+          <p
             class="
               block
               sm:px-5 sm:py-0
-              py-2
+              py-3
               text-lg
               hover:cursor-pointer hover:text-teal-400
               transition-colors
               duration-300
             "
-            >Projets</router-link
           >
+            Projets
+          </p>
           <div>
             <a
               href="mailto:mathis.dousse@outlook.fr"
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import PresentationView from "../views/PresentationView";
-import MyProjects from "@/components/MyProjects.vue";
 
 const routes = [
   {
@@ -8,7 +7,6 @@ const routes = [
     name: "Presentationview",
     component: PresentationView,
   },
-  { path: "/", name: "Projects", component: MyProjects },
 ];
 
 const router = createRouter({
