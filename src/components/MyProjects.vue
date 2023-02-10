fe537367038e4f56136ffff8dc640be10ab2ad5c
Mathis Dousse <41020659+FeckNeck@users.noreply.github.com>
2023-02-10 12:28:29 +0100

    fix: project component width

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -2,7 +2,7 @@
   <section class="py-14" id="projects">
     <div
       class="
-        xl:px-24 xl:max-w-5xl
+        xl:px-24 xl:max-w-7xl
         lg:max-w-5xl lg:px-20
         md:max-w-5xl
         sm:max-w-2xl sm:px-8
