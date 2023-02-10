426e575edf44ba8ba80e85650b347fccb7511723
Mathis Dousse <41020659+FeckNeck@users.noreply.github.com>
2023-02-10 12:29:45 +0100

    fix: custom shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,19 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        custom: "0px 0px 10px 1px rgb(155 153 153 / 75%)",
+        custom: "0 2px 4px rgba(0,0,0,0.18)",
       },
       translate: {
         up: "0.25em",
       },
       colors: {
         'primary-light': '#F7F8FC',
-				'secondary-light': '#FFFFFF',
-				'ternary-light': '#f6f7f8',
+	'secondary-light': '#FFFFFF',
+	'ternary-light': '#f6f7f8',
 
-				'primary-dark': '#0D2438',
-				'secondary-dark': '#102D44',
-				'ternary-dark': '#1E3851',
+	'primary-dark': '#0D2438',
+	'secondary-dark': '#102D44',
+	'ternary-dark': '#1E3851',
       },
       transitionProperty:{
         'bg':'background-color'
